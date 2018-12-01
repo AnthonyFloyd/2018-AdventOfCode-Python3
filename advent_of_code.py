1aa83c0e3ef37b96751a8ce3b972238ae37f19a2
Anthony Floyd <anthonyfloyd@gmail.com>
2018-12-01 11:02:37 -0800

    Added type hinting. Not sure I like it.

--- advent_of_code.py
+++ advent_of_code.py
@@ -1,27 +1,29 @@
 # Advent of Code "Library"
+'''Omnibus library for Advent of Code 2018'''
+
+from typing import List, Callable, Any
 
 from day01 import accumulateFromList, findFirstDuplicate
 
-def printBoilerPlate(day, description):
+def printBoilerPlate(day:str, description:str) -> None:
     '''Write out a boilerplate header for the Advent of Code trials.
 
     '''
 
-    dayString = f'Day {day}: {description}'
-    separator = '~' * len(dayString)
+    dayString:str = f'Day {day}: {description}'
+    separator:str = '~' * len(dayString)
     print('Advent of Code 2018')
     print(dayString)
     print(separator)
 
-def readFileToList(filename, transformation=lambda x: x):
+def readFileToList(filename:str, transformation=lambda x: x) -> List[Any]:
     '''Reads an AoC input file into a list, performing the optional transformation
     on each list element, after stripping the element.
 
     '''
 
     with open(filename) as inputFile:
-        inputLines = inputFile.readlines()
-        inputList = [transformation(i.strip()) for i in inputLines]
+        inputLines:List[str] = inputFile.readlines()
+        inputList:List[Any] = [transformation(i.strip()) for i in inputLines]
 
     return inputList
-
--- day01.py
+++ day01.py
@@ -4,23 +4,25 @@ Day 1: Chronal Calibration
 
 '''
 
+from typing import List, Dict
+
 DAY = '1'
 NAME = 'Chronal Calibration'
 INPUT_FILE = 'day01-input.txt'
 
-def accumulateFromList(intList):
+def accumulateFromList(intList:List[int]) -> int:
     '''Sum a list of ints.
 
     Takes a list of positive and negative ints, returns an int.
     '''
 
-    total = 0
+    total:int = 0
     for i in intList:
         total += i
 
     return total
 
-def findFirstDuplicate(intList):
+def findFirstDuplicate(intList:List[int]) -> int:
     '''Find the first repeated value in the process of summing a list of ints.
 
     The int list may be repeated until a duplicate is found.
@@ -28,11 +30,11 @@ def findFirstDuplicate(intList):
     Takes a list of positive and negative ints, returns an int.
     '''
 
-    totals = {}
-    total = 0
+    totals:Dict[int, int] = {}
+    total:int = 0
 
     totals[total] = 1
-    noDuplicate = True
+    noDuplicate:bool = True
 
     while noDuplicate:
         for i in intList:
@@ -46,7 +48,7 @@ def findFirstDuplicate(intList):
 
     return total
 
-def run():
+def run() -> None:
     '''Run both part 1 and part 2 for AoC2018 Day 1.
 
     '''
@@ -56,16 +58,16 @@ def run():
     printBoilerPlate(DAY, NAME)
 
     # gather inputs
-    inputList = readFileToList(INPUT_FILE, transformation=int)
+    inputList:List[int] = readFileToList(INPUT_FILE, transformation=int)
 
     # Part 1
     print('\nPart 1')
-    finalFrequency = accumulateFromList(inputList)
+    finalFrequency:int = accumulateFromList(inputList)
     print(f'Final frequency: {finalFrequency}')
 
     # Part 2
     print('\nPart 2')
-    firstDuplicate = findFirstDuplicate(inputList)
+    firstDuplicate:int = findFirstDuplicate(inputList)
     print(f'First duplicate frequency: {firstDuplicate}')
 
 if __name__ == '__main__':
